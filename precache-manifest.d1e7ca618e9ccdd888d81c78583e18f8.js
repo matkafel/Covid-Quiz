86c5cfdfdde2f72f13bfe91968ed21bf25623926
unknown <mat.kafel@gmail.com>
2020-05-12 09:54:07 +0200

    Updates

---
+++ precache-manifest.d1e7ca618e9ccdd888d81c78583e18f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3299856d69acac5652a27af6727191a",
+    "url": "/Covid-Quiz/index.html"
+  },
+  {
+    "revision": "1717e436672599d930fc",
+    "url": "/Covid-Quiz/static/css/main.ccf8c819.chunk.css"
+  },
+  {
+    "revision": "9a824b65c6a42dbbfb57",
+    "url": "/Covid-Quiz/static/js/2.0cb42c82.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Covid-Quiz/static/js/2.0cb42c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1717e436672599d930fc",
+    "url": "/Covid-Quiz/static/js/main.c8d235af.chunk.js"
+  },
+  {
+    "revision": "6bfb9c07732f5180bd31",
+    "url": "/Covid-Quiz/static/js/runtime-main.41bc60c0.js"
+  },
+  {
+    "revision": "78a43eaa3b01091f0ddc2b5ce9e2c62c",
+    "url": "/Covid-Quiz/static/media/1.78a43eaa.svg"
+  },
+  {
+    "revision": "cae0fb0baaeeb7eec12d79b957912c62",
+    "url": "/Covid-Quiz/static/media/bg.cae0fb0b.png"
+  }
+]);
