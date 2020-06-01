f8b88025a5b6010790eb509997cbf93c997ff8dd
unknown <mat.kafel@gmail.com>
2020-06-01 12:55:47 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import App from './components/App';
 
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Route component={App} />
+  </Router>,
   document.getElementById('root')
 );
 
